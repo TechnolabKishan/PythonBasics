0f1ae49eec4083aaa8f9a96ba9b9bb4e8a7e2813
kishan <Kishan@technolableiden.nl>
2025-03-14 11:12:09 +0100

    modules import systeem

---
+++ modules.py
@@ -0,0 +1,16 @@
+# module = a file containing code you want to include in your program
+# use 'import' to include it in
+import maths
+# import math as Rekenen
+# from math import pi
+
+
+result = maths.pi
+result = maths.square(3)
+result = maths.cube(3)
+result = maths.circumference(3)
+result = maths.area(3)
+
+
+
+print(result)
