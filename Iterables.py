960d5b33880ba60b078aa35a121c5085ad274724
kishan <Kishan@technolableiden.nl>
2025-03-14 09:38:10 +0100

    Iterables gemaakt met sets{} en tuple()  en list[]

---
+++ Iterables.py
@@ -0,0 +1,23 @@
+# Iterables = an object/collection that can returen its elements one
+# at a time, allowing it to be iterated over in a loop
+
+# numbers = (1, 2, 3, 4, 5)
+
+
+# for i in reversed(numbers):
+#     print(i)
+
+# fruits = {'banaan', 'appel,', 'kiwi', 'annas'} # sets kan je niet reversen
+
+# for fruit in fruits:
+#     print(fruit)
+
+# name = 'Elmo'
+
+# for charachter in name:
+#     print(charachter, end=' ')
+
+my_dictionary = {'A': 1, "B": 2, "C": 3}
+
+for key, value in my_dictionary.items():
+    print(f" {key} = {value}")
